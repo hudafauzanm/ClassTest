d87538a5734baa502b74eb9dbe73ed2683e5f268
Huda Fauzan Murtadho <hudafauzanm@gmail.com>
2020-01-15 16:38:16 +0700

    Log Progress 0.1

---
+++ Log/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Log
+{
+    public class Log
+    {
+        private string _source;
+
+        public static string emerg(string _source)
+        {
+
+        }
+        public static string alert(string _source)
+        {
+
+        }
+
+        public static string crit(string _source)
+        {
+
+        }
+
+        public static string err(string _source)
+        {
+
+        }
+
+        public static string warning(string _source)
+        {
+
+        }
+
+        public static string notice(string _source)
+        {
+
+        }
+
+        public static string info(string _source)
+        {
+
+        }
+
+        public static string debug(string _source)
+        {
+
+        }
+
+        public static string log ()
+        {
+            
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
