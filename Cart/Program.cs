004da489d3ca791af58d344e11c9410e53437db9
Huda Fauzan Murtadho <hudafauzanm@gmail.com>
2020-01-16 14:33:55 +0700

    Cart Done

--- Cart/Program.cs
+++ Cart/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using Carts;
 
-namespace Cart
+namespace Carto
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Cart cart = new Cart();
+            cart.addItem(1,30000,3)
+                .addItem(2,10000)
+                .addItem(3,5000,2)
+                .removeItem(2)
+                .addItem(4,400,6)
+                .addDiscount("50%");
+            cart.showAll();
+            Console.WriteLine(cart.TotalItem());
+            Console.WriteLine(cart.totalQuantity());
+            Console.WriteLine(cart.totalPrice());
+            
+            cart.checkout();
         }
     }
 }
